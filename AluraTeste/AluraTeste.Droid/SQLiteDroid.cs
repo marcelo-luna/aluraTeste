4c4e29554045322bbbec8353a3b616757b9ffd95
marcelo.luna <marcelo.1.luna@gmail.com>
2016-12-28 08:28:10 -0200

    Banco de dados

--- AluraTeste/AluraTeste.Droid/SQLiteDroid.cs
+++ AluraTeste/AluraTeste.Droid/SQLiteDroid.cs
@@ -17,10 +17,19 @@ namespace AluraTeste.Droid
         public SQLiteConnection GetConnection()
         {
             var fileName = "Refeicoes.db3";
-            var documents = Environment.GetFolderPath(Environment.SpecialFolder.Personal);
-            var path = Path.Combine(documents, "..", "Library", fileName);
 
-            return  new SQLiteConnection(path);
+            string applicationFolderPath = System.IO.Path.Combine(System.Environment.GetFolderPath(System.Environment.SpecialFolder.Personal), "Refeicao.db");
+            // Create the folder path.
+            System.IO.Directory.CreateDirectory(applicationFolderPath);
+
+            string databaseFileName = System.IO.Path.Combine(applicationFolderPath, "Refeicao.db");
+            //SQLite.SQLite3.Config(SQLite.SQLite3.ConfigOption.Serialized);
+            var db = new SQLiteConnection(databaseFileName);
+
+            //var documents = Environment.GetFolderPath(Environment.SpecialFolder.Personal);
+            //var path = Path.Combine(documents, "data", "data", fileName);
+
+            return  new SQLiteConnection(databaseFileName);
         }
     }
 }
